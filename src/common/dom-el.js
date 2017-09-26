9aab19627932f5e332cb738b91d8c3053abf1cc0
Tomas Polach <tomas@polach.ch>
2017-09-26 19:40:43 +0200

    add el.empty() method

--- src/common/dom-el.js
+++ src/common/dom-el.js
@@ -64,6 +64,10 @@ function extendWithConvenienceMethods (el) {
     child ? el.removeChild(child) : el.parentNode.removeChild(el)
     return el
   }
+  el.empty = function emptyElement () {
+    while (el.lastChild) el.removeChild(el.lastChild)
+    return el
+  }
   el.append = function append (o) {
     if (o) isElement(o) ? el.appendChild(o) : el.innerHTML = o
     return el
