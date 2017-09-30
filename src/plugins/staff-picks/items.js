f0ddd7f74ca7afb4e8b6fb74feb82372487c70a3
Tomas Polach <tomas@polach.ch>
2017-09-30 09:43:29 +0200

    updates item list

--- src/plugins/staff-picks/items.js
+++ src/plugins/staff-picks/items.js
@@ -26,4 +26,106 @@ export default [{
   thumb: null,
   url: 'https://storage.3d.io/535e624259ee6b0200000484/170927-2018-a10bpi/archilogic_2017-09-27_20-18-12_GT3EQI.gz.data3d.buffer',
   format: 'data3d'
+},{
+  title: 'Loewy House by Albert Frey HalfSkyCylinder',
+  thumb: 'https://storage.3d.io/535e624259ee6b0200000484/2017-09-29_12-36-00_pGKpVA/Cursor_and_Archilogic.png',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/wlnobthy?modelResourceId=88ac69d1-28c1-406c-890c-1f86531e2a31&mode=edit',
+  url: 'https://storage.3d.io/535e624259ee6b0200000484/170929-1444-iae715/archilogic_2017-09-29_14-42-24_3Xo4kP.gz.data3d.buffer',
+  format: 'data3d'
+},{
+  title: '',
+  thumb: '',
+  editorUrl: '',
+  url: '',
+  format: 'data3d'
+},{
+  title: 'Stahl House',
+  thumb: '',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/la7jhe3t?modelResourceId=216cfcbc-82d3-4ad0-a834-e6cfbb173dcb&mode=edit',
+  url: '',
+  format: 'data3d'
+},{
+  title: 'The Office',
+  thumb: '',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/uhyxqfvr?modelResourceId=34bfdbba-a44d-4268-bdb8-6b7cfe719555',
+  url: '',
+  format: 'data3d'
+},{
+  title: 'The Office Opacity',
+  thumb: '',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/jvafqyqy?modelResourceId=4cf23256-ed41-4fc0-96b1-e495e93e3da0&mode=edit',
+  url: '',
+  format: 'data3d'
+},{
+  title: 'Ryerson Student Learning Center',
+  thumb: '',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/gnrn8dun?modelResourceId=6b02c7de-31bb-4641-af5b-52a9b1c334c4&mode=edit',
+  url: '',
+  format: 'data3d'
+},{
+  title: '221B Baker Street - Sherlock\'s apartment',
+  thumb: '',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/1i0vuotf?modelResourceId=de9457f2-74e1-4acf-b229-fb304093ef2e&mode=edit',
+  url: '',
+  format: 'data3d'
+},{
+  title: 'Big Bang Theory',
+  thumb: '',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/fljox97z?modelResourceId=60dde7c8-d481-475f-9c0f-8ecbe57cef05&mode=edit',
+  url: '',
+  format: 'data3d'
+},{
+  title: 'Christian Grey\'s Apartment',
+  thumb: '',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/dekt1bq3?modelResourceId=6d511fe1-6189-452b-b822-510d42b74e72&mode=edit',
+  url: '',
+  format: 'data3d'
+},{
+  title: 'Carrie Bradshaw\'s Apartment',
+  thumb: '',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/fpt7d8rj?modelResourceId=77123cb8-ceb2-4bd7-8e59-9ee3feae7441&mode=edit',
+  url: '',
+  format: 'data3d'
+},{
+  title: 'Friends',
+  thumb: '',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/9ah60id5?modelResourceId=cf159e73-4f7a-413b-adb2-563913ba4fd7&mode=edit',
+  url: '',
+  format: 'data3d'
+},{
+  title: 'Aluminaire_House',
+  thumb: '',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/0imo45dd?modelResourceId=cff45650-49e6-43d2-993e-7076d6b07232&mode=edit',
+  url: '',
+  format: 'data3d'
+},{
+  title: 'Miller & Monroe House by Frank Lloyd Wright ',
+  thumb: '',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/qlnxw5bm?modelResourceId=ba644605-277f-4a12-bc9e-13cbc51e94e2&mode=edit',
+  url: '',
+  format: 'data3d'
+},{
+  title: 'Case Study House 4, Ralph Rapson',
+  thumb: '',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/krxvla7n?modelResourceId=ac682b97-3c9b-45a0-9c90-f1534ea769d3&mode=edit',
+  url: '',
+  format: 'data3d'
+},{
+  title: '55 West 17th Street @night',
+  thumb: '',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/empzwgkx?modelResourceId=e717c8e5-3d7c-423b-b2f2-21874665a5b3&mode=edit&view-menu=none&main-menu=save&logo=false',
+  url: '',
+  format: 'data3d'
+},{
+  title: 'Steve Jobs Theatre',
+  thumb: '',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/clxtnlsv?modelResourceId=6f3a27fa-51e0-4773-9cb7-87d497b554af&mode=edit',
+  url: '',
+  format: 'data3d'
+},{
+  title: 'The Performance Arcade',
+  thumb: '',
+  editorUrl: 'https://spaces.archilogic.com/model/archilogic/rqjxqjug?modelResourceId=6dc5516a-1c90-4204-a572-bfb45beec3fe&mode=edit',
+  url: '',
+  format: 'data3d'
 }]
